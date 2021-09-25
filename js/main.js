c14404895282ea188fdc70c63cd253203d968b41
Gaah22gavin <89858687+Gaah22gavin@users.noreply.github.com>
2021-09-25 10:22:07 +0200

    first

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(document).on('ready', e=>{
+    $('.line').on('click', e=>{
+        $('.menu').toggleClass("line-open")
+        $('header').toggleClass('open');
+    });
+})
+
+// ===================================================
+
+function openCity(cityName, elmnt, color) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+
+    document.getElementById(cityName).style.display = "block";
+
+    elmnt.style.backgroundColor = color;
+}
+  
+document.getElementById("defaultOpen").click();
+
+// ====================================================
